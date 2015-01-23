8444aa02f4ee483177ba839ac3a5777e446ee7a7
Martin <martianus@users.noreply.github.com>
2015-01-23 11:38:33 +0100

    Revert "Disable generation of jira init script"
    This reverts commit 461291c8394decbe4755fe9a5055a6b797d3ed10.

--- recipes/sysv.rb
+++ recipes/sysv.rb
@@ -1,8 +1,8 @@
-#template '/etc/init.d/jira' do
-#  source 'jira.init.erb'
-#  mode '0755'
-#  notifies :restart, 'service[jira]', :delayed
-#end
+template '/etc/init.d/jira' do
+  source 'jira.init.erb'
+  mode '0755'
+  notifies :restart, 'service[jira]', :delayed
+end
 
 service 'jira' do
   supports :status => :true, :restart => :true
