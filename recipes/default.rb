f8e482a232e3e5bf5c18d7bde86b84a1186b3565
Kleijn, M van der - KLM <Martijn-van-der.Kleijn@klm.com>
2015-10-14 17:32:54 +0200

    Rollback accidental delete of line.
    See https://github.com/afklm/chef_jira/pull/33#discussion-diff-42007416

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,7 @@
 settings = Jira.settings(node)
 
 include_recipe 'chef_jira::database' if settings['database']['host'] == '127.0.0.1'
+include_recipe "chef_jira::#{node['jira']['install_type']}"
 include_recipe 'chef_jira::configuration'
 include_recipe 'chef_jira::container_server_configuration'
 include_recipe 'chef_jira::apache2'
