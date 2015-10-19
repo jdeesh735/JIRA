a3899a6d25cacc5ab6caf10654e719d092a79757
Mikhail Zholobov <legal90@gmail.com>
2015-10-19 15:02:02 +0300

    Remove ['jira]['update'] attribute and detect JIRA version automatically

--- recipes/installer.rb
+++ recipes/installer.rb
@@ -1,20 +1,23 @@
-template "#{Chef::Config[:file_cache_path]}/atlassian-jira-response.varfile" do
-  source 'response.varfile.erb'
-  owner 'root'
-  group 'root'
-  mode '0644'
-end
+if jira_version != node['jira']['version']
+  template "#{Chef::Config[:file_cache_path]}/atlassian-jira-response.varfile" do
+    source 'response.varfile.erb'
+    owner 'root'
+    group 'root'
+    mode '0644'
+    variables(
+      'update' => Dir.exist?(node['jira']['install_path'])
+    )
+  end
 
-remote_file "#{Chef::Config[:file_cache_path]}/atlassian-jira-#{node['jira']['version']}-#{node['jira']['arch']}.bin" do
-  source jira_artifact_url
-  checksum jira_artifact_checksum
-  mode '0755'
-  action :create_if_missing
-  notifies :run, "execute[Installing Jira #{node['jira']['version']}]", :immediately
-end
+  remote_file "#{Chef::Config[:file_cache_path]}/atlassian-jira-#{node['jira']['version']}-#{node['jira']['arch']}.bin" do
+    source jira_artifact_url
+    checksum jira_artifact_checksum
+    mode '0755'
+    action :create
+  end
 
-execute "Installing Jira #{node['jira']['version']}" do
-  cwd Chef::Config[:file_cache_path]
-  command "./atlassian-jira-#{node['jira']['version']}-#{node['jira']['arch']}.bin -q -varfile atlassian-jira-response.varfile"
-  not_if { node['jira']['update'] == false && ::File.directory?("#{node['jira']['install_path']}") }
+  execute "Installing Jira #{node['jira']['version']}" do
+    cwd Chef::Config[:file_cache_path]
+    command "./atlassian-jira-#{node['jira']['version']}-#{node['jira']['arch']}.bin -q -varfile atlassian-jira-response.varfile"
+  end
 end
