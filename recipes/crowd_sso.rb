da49d4b82452d822042027b3038577935680efe0
Kleijn, M van der - KLM <Martijn-van-der.Kleijn@klm.com>
2015-10-21 12:04:05 +0200

    Fix style issue

--- recipes/crowd_sso.rb
+++ recipes/crowd_sso.rb
@@ -17,7 +17,7 @@ end
 # See: https://confluence.atlassian.com/display/CROWD/Integrating+Crowd+with+Atlassian+JIRA
 file_replace_line "#{node['jira']['install_path']}/atlassian-jira/WEB-INF/classes/seraph-config.xml" do
   replace '    <authenticator class="com.atlassian.jira.security.login.JiraSeraphAuthenticator"/>'
-  with    '    <authenticator class="com.atlassian.jira.security.login.SSOSeraphAuthenticator"/>'
-  notifies :restart, "service[jira]", :delayed
+  with '    <authenticator class="com.atlassian.jira.security.login.SSOSeraphAuthenticator"/>'
+  notifies :restart, 'service[jira]', :delayed
   only_if { node['jira']['crowd_sso']['enabled'] == true }
 end
