40cbef34f96c06bb201d5d881377dde08ed49230
Kleijn, M van der - KLM <Martijn-van-der.Kleijn@klm.com>
2015-12-18 13:22:03 +0100

    Fix Foodcritic and Rubocop issues

--- recipes/autotune.rb
+++ recipes/autotune.rb
@@ -19,54 +19,54 @@
 tune_type = 'mixed'
 
 # Check if type is selected and if its a valid type
-if (node['jira'].attribute?('autotune') && node['jira']['autotune'].attribute?('type'))
+if node['jira'].attribute?('autotune') && node['jira']['autotune'].attribute?('type')
   tune_type = node['jira']['autotune']['type']
 
-  if (!(['mixed','dedicated','shared'].include?(tune_type)))
+  unless %w(mixed dedicated shared).include?(tune_type)
     Chef::Log.fatal([
-        "Bad value (#{tune_type}) for node['jira']['autotune']['type'] attribute.",
-        "Valid values are one of mixed, dedicated, shared."
-      ].join(' '))
-    raise
+      "Bad value (#{tune_type}) for node['jira']['autotune']['type'] attribute.",
+      'Valid values are one of mixed, dedicated, shared.'
+    ].join(' '))
+    fail
   end
 end
 
 # Parse out total_memory option, or use value detected by Ohai.
 total_memory = node['memory']['total']
 
-if (node['jira'].attribute?('autotune') && node['jira']['autotune'].attribute?('total_memory'))
+if node['jira'].attribute?('autotune') && node['jira']['autotune'].attribute?('total_memory')
   total_memory = node['jira']['autotune']['total_memory']
-  if (total_memory.match(/\A\d*kB\Z/) == nil)
+  if total_memory.match(/\A\d*kB\Z/).nil?
     Chef::Application.fatal!([
-        "Bad value (#{total_memory}) for node['jira']['autotune']['total_memory'] attribute.",
-        "Valid values are non-zero integers followed by kB (e.g., 49416564kB)."
-      ].join(' '))
+      "Bad value (#{total_memory}) for node['jira']['autotune']['total_memory'] attribute.",
+      'Valid values are non-zero integers followed by kB (e.g., 49416564kB).'
+    ].join(' '))
   end
 end
 
 # Ohai reports node[:memory][:total] in kB, as in "921756kB"
-mem = total_memory.split("kB")[0].to_i / 1024 # in MB
+mem = total_memory.split('kB')[0].to_i / 1024 # in MB
 
 maximum_memory =
-  { "mixed" => (mem / 100) * 70,
-    "dedicated" => (mem / 100) * 80,
-    "shared" => (mem / 100) * 50
+  { 'mixed' => (mem / 100) * 70,
+    'dedicated' => (mem / 100) * 80,
+    'shared' => (mem / 100) * 50
   }.fetch(tune_type)
 
-node.default['jira']['jvm']['maximum_memory'] = normalize(maximum_memory*1024*1024)
+node.default['jira']['jvm']['maximum_memory'] = normalize(maximum_memory * 1024 * 1024)
 Chef::Log.warn("Autotuning JIRA max memory to #{node['jira']['jvm']['maximum_memory']}.")
 
 minimum_memory =
-  { "mixed" => (maximum_memory / 100) * 80,
-    "dedicated" => maximum_memory,
-    "shared" => (maximum_memory / 100) * 50
+  { 'mixed' => (maximum_memory / 100) * 80,
+    'dedicated' => maximum_memory,
+    'shared' => (maximum_memory / 100) * 50
   }.fetch(tune_type)
 
-node.default['jira']['jvm']['minimum_memory'] = normalize(minimum_memory*1024*1024)
+node.default['jira']['jvm']['minimum_memory'] = normalize(minimum_memory * 1024 * 1024)
 Chef::Log.warn("Autotuning JIRA min memory to #{node['jira']['jvm']['minimum_memory']}.")
 
 # Lets make sure we have at least 512 MB
-if (minimum_memory < 512)
-  Chef::Log.fatal("Autotune reports less than 512 MB available for JIRA, please make at least 512 MB memory available.")
-  raise
+if minimum_memory < 512
+  Chef::Log.fatal('Autotune reports less than 512 MB available for JIRA, please make at least 512 MB memory available.')
+  fail
 end
